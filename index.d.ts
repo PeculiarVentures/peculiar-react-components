fbe93fe418742a2d48673d2d23939bcfd816e8a3
donskov <donskovdmitriyv@gmail.com>
2018-11-28 15:50:13 +0200

    fix declaration

--- index.d.ts
+++ index.d.ts
@@ -110,12 +110,12 @@ interface CircularProgressProps extends React.HTMLAttributes<HTMLDivElement> {
   ref?: React.Ref<HTMLElement>;
 }
 
-interface CounterComponentProps extends React.HTMLAttributes<HTMLDivElement> {
+interface CounterProps {
   value?: number;
   defaultValue?: number;
   minValue: number;
   maxValue: number;
-  onChange?: void;
+  onChange: (value: number) => void;
   minValueLabel?: number | string;
   maxValueLabel?: number | string;
   disabled?: boolean;
@@ -432,7 +432,7 @@ declare const Checkbox: React.ComponentType<CheckboxProps>;
 declare const ContentLoader: React.ComponentType<ContentLoaderProps>;
 declare const HighlightCode: React.ComponentType<HighlightCodeProps>;
 declare const CircularProgress: React.ComponentType<CircularProgressProps>;
-declare const Counter: React.ComponentType<CounterComponentProps>;
+declare const Counter: React.ComponentType<CounterProps>;
 declare const LinearProgress: React.ComponentType<LinearProgressProps>;
 declare const ProgressiveImage: React.ComponentType<ProgressiveImageProps>;
 declare const Radio: React.ComponentType<RadioProps>;
