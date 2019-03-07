a07aa11d75060aad80bb7a12a88e515383d8a4ab
donskov <donskovdmitriyv@gmail.com>
2019-03-07 19:34:04 +0200

    chore(content loader): Move content loader component to class

--- lib/components/content_loader/index.jsx
+++ lib/components/content_loader/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import uuid from '../../utils/uuid';
@@ -6,84 +6,87 @@ import uuid from '../../utils/uuid';
 /**
  * ContentLoader component
  */
-export default function ContentLoader(props) {
-  const idClip = uuid();
-  const idGradient = uuid();
-  const {
-    color,
-    duration,
-    className,
-    height,
-    width,
-    children,
-    ...other
-  } = props;
+export default class ContentLoader extends PureComponent {
+  static propTypes = {
+    /**
+     * Color from theme
+     */
+    color: PropTypes.string,
+    /**
+     * Animation duration
+     */
+    duration: PropTypes.number,
+    /**
+     * The CSS class name of the root element
+     */
+    className: PropTypes.string,
+    /**
+     * Height of component in px
+     */
+    height: PropTypes.number,
+    /**
+     * Width of component in px
+     */
+    width: PropTypes.number,
+    /**
+     * The content of the component
+     */
+    children: PropTypes.node.isRequired,
+  };
 
-  return (
-    <svg
-      data-component="content-loader"
-      viewBox={`0 0 ${width} ${height}`}
-      className={classNames(
-        'content_loader',
-        className,
-      )}
-      {...other}
-    >
-      <rect
-        style={{ fill: `url(#${idGradient})` }}
-        clipPath={`url(#${idClip})`}
-        x="0"
-        y="0"
-        width={width}
-        height={height}
-      />
+  static defaultProps = {
+    color: 'grey',
+    duration: 2,
+    className: '',
+    height: 300,
+    width: 300,
+  };
 
-      <defs>
-        <clipPath id={idClip}>
-          {children}
-        </clipPath>
+  idClip = uuid();
+  idGradient = uuid();
 
-        <linearGradient id={idGradient}>
-          <stop offset="0%" className={`content_loader_color_${color}`} />
-          <stop offset="50%" className={`content_loader_color_${color}`} />
-          <stop offset="100%" className={`content_loader_color_${color}`} />
-        </linearGradient>
-      </defs>
-    </svg>
-  );
+  render() {
+    const {
+      color,
+      duration,
+      className,
+      height,
+      width,
+      children,
+      ...other
+    } = this.props;
+
+    return (
+      <svg
+        data-component="content-loader"
+        viewBox={`0 0 ${width} ${height}`}
+        className={classNames(
+          'content_loader',
+          className,
+        )}
+        {...other}
+      >
+        <rect
+          style={{ fill: `url(#${this.idGradient})` }}
+          clipPath={`url(#${this.idClip})`}
+          x="0"
+          y="0"
+          width={width}
+          height={height}
+        />
+
+        <defs>
+          <clipPath id={this.idClip}>
+            {children}
+          </clipPath>
+
+          <linearGradient id={this.idGradient}>
+            <stop offset="0%" className={`content_loader_color_${color}`} />
+            <stop offset="50%" className={`content_loader_color_${color}`} />
+            <stop offset="100%" className={`content_loader_color_${color}`} />
+          </linearGradient>
+        </defs>
+      </svg>
+    );
+  }
 }
-
-ContentLoader.propTypes = {
-  /**
-   * Color from theme
-   */
-  color: PropTypes.string,
-  /**
-   * Animation duration
-   */
-  duration: PropTypes.number,
-  /**
-   * The CSS class name of the root element
-   */
-  className: PropTypes.string,
-  /**
-   * Height of component in px
-   */
-  height: PropTypes.number,
-  /**
-   * Width of component in px
-   */
-  width: PropTypes.number,
-  /**
-   * The content of the component
-   */
-  children: PropTypes.node.isRequired,
-};
-
-ContentLoader.defaultProps = {
-  color: 'grey',
-  duration: 2,
-  className: '',
-  height: 300,
-  width: 300,
-};
