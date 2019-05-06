87ec8a20253931dcc216b53caf0947365602a334
Bonn2018 <henkbuk@gmail.com>
2019-05-06 22:11:08 +0300

    chore: removed extra styles

--- lib/components/checkbox/index.jsx
+++ lib/components/checkbox/index.jsx
@@ -42,7 +42,6 @@ export default class Checkbox extends SwitchHandler {
       iconColor,
       iconColorOn,
       iconType,
-      classNameOn,
       ...other
     } = this.props;
     const { checkedState } = this.state;
