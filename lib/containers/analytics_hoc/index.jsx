8ac19b4f84f4e756dff7c5731d3c3fc5dafbb327
WorldThirteen <mihailzachepilo@gmail.com>
2019-07-31 14:32:04 +0300

    add anlytics HOC

---
+++ lib/containers/analytics_hoc/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function withAnalytics(Component, firingEventProp = 'onClick') {
+  const AnalyticsWrapper = ({ forwardRef, gaEventName, ...props }, context) => {
+    const oldProp = props[firingEventProp];
+    const fireEvent = (...args) => {
+      if (context.gaFireEvent && gaEventName) {
+        context.gaFireEvent(gaEventName);
+      }
+      oldProp && oldProp(...args);
+    };
+
+    return (
+      <Component
+        {...props}
+        {...{ [firingEventProp]: fireEvent }}
+        ref={forwardRef}
+      />
+    );
+  }
+
+  const WrappedComponent = React.forwardRef((props, ref) => {
+    return <AnalyticsWrapper
+      {...props}
+      forwardRef={ref}
+    />;
+  });
+
+  WrappedComponent.name = Component.name;
+  AnalyticsWrapper.contextTypes = {
+    gaFireEvent: PropTypes.func,
+  };
+
+  return WrappedComponent;
+}
