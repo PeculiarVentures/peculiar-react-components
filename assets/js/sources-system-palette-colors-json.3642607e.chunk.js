fb0bd592b142931862cb8cb0018c391a751fee54
donskov <donskovdmitriyv@gmail.com>
2020-08-28 08:39:23 +0000

    Updates

---
+++ assets/js/sources-system-palette-colors-json.3642607e.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[105],{271:function(e){e.exports=JSON.parse('["primary","secondary","black","dark_grey","grey","light_grey","success","wrong","white"]')}}]);
+//# sourceMappingURL=sources-system-palette-colors-json.3642607e.chunk.js.map
--- assets/js/sources-system-palette-index-sass.5724df83.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[107],{271:function(o,e,n){o.exports={color_item:"color_item_2HjZI",color_name:"color_name_j-zwe"}}}]);
-//# sourceMappingURL=sources-system-palette-index-sass.5724df83.chunk.js.map
