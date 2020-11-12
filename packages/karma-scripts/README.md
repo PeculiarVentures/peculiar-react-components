cca0a616f3980cfdb525d38b5ec9e0833cf91004
donskov <donskovdmitriyv@gmail.com>
2020-11-12 14:07:28 +0200

    feat: add package `karma-scripts`

---
+++ packages/karma-scripts/README.md
@@ -0,0 +1,42 @@
+# Karma scripts
+
+## Usage
+
+`karma.conf.js`
+```js
+const { createKarmaConfig } = require('@peculiar/karma-scripts');
+
+module.exports = (config) => {
+  const baseConfig = createKarmaConfig({
+    dirname: __dirname,
+    coverageExcludes: [],
+  });
+
+  config.set(baseConfig);
+  config.set({
+    // overrides here
+  });
+};
+```
+
+`test/index.ts`
+```ts
+import '../src/my.spec';
+```
+
+`test/index.ts` (with React)
+```ts
+import '@peculiar/karma-scripts/enzymeAdapter';
+
+import '../src/my.spec';
+```
+
+`package.json`
+```json
+{
+  "scripts": {
+    "test:karma": "karma start",
+    "test:karma:debug": "karma start --single-run=false --reporters=mocha --debug"
+  }
+}
+```
---
+++ packages/karma-scripts/createKarmaConfig.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const webpackConfig = require('@peculiar/webpack-scripts/webpack.config.karma');
+
+const COVERAGE_PERCENT = 80;
+const COVERAGE_PERCENT_HIGH = 90;
+const KARMA_SERVER_PORT = 9876;
+
+module.exports = function createKarmaConfig({
+  coverage = true, dirname, coverageExcludes, coverageOverrides,
+}) {
+  const config = {
+    basePath: dirname,
+    frameworks: [
+      'mocha',
+    ],
+    browsers: [
+      'ChromeHeadlessWithGPU',
+    ],
+    customLaunchers: {
+      ChromeHeadlessWithGPU: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--headless',
+          '--hide-scrollbars',
+          '--mute-audio',
+        ],
+      },
+    },
+    browserDisconnectTimeout: 120000,
+    browserDisconnectTolerance: 1,
+    browserNoActivityTimeout: 300000,
+    colors: true,
+    port: KARMA_SERVER_PORT,
+    files: [
+      path.join(dirname, 'test/index.ts'),
+    ],
+    preprocessors: {
+      [path.join(dirname, 'test/index.ts')]: ['webpack'],
+    },
+    reporters: [
+      'mocha',
+    ],
+    coverageReporter: {
+      check: {
+        each: {
+          lines: COVERAGE_PERCENT,
+          statements: COVERAGE_PERCENT,
+          excludes: coverageExcludes,
+          overrides: coverageOverrides,
+        },
+      },
+      includeAllSources: true,
+      type: 'lcov',
+      watermarks: {
+        lines: [COVERAGE_PERCENT, COVERAGE_PERCENT_HIGH],
+        statements: [COVERAGE_PERCENT, COVERAGE_PERCENT_HIGH],
+      },
+    },
+    singleRun: true,
+    webpack: webpackConfig,
+    webpackMiddleware: {
+      noInfo: true,
+      stats: {
+        children: false,
+        chunks: false,
+      },
+    },
+  };
+
+  if (coverage) {
+    config.reporters.push(
+      'coverage',
+    );
+  }
+
+  return config;
+};
