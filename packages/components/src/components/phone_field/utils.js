375b3a9cd445e2b824e8c8a2b92b8fd1ae447741
donskov <donskovdmitriyv@gmail.com>
2021-01-26 16:34:59 +0200

    feat(components): add countries data to phone field component

---
+++ packages/components/src/components/phone_field/utils.js
@@ -0,0 +1,64 @@
+import rawCountries from './raw_countries';
+
+export const getMask = (prefix, dialCode, predefinedMask, defaultMask) => {
+  if (!predefinedMask) {
+    return `${prefix}${''.padEnd(dialCode.length, '.')} ${defaultMask}`;
+  }
+
+  return `${prefix}${''.padEnd(dialCode.length, '.')} ${predefinedMask}`;
+};
+
+const prefix = '+';
+
+export const countries = rawCountries.map(country => ({
+  name: country[0],
+  regions: country[1],
+  iso2: country[2].toUpperCase(),
+  countryCode: country[3],
+  dialCode: `${prefix}${country[3]}`,
+  format: getMask(prefix, country[3], country[4], '... ... ... ... ..'),
+}));
+
+export const formatNumber = (text, country) => {
+  if (!country) {
+    return text;
+  }
+
+  const { format } = country;
+  const pattern = format;
+
+  if (!text || text.length === 0) {
+    return prefix;
+  }
+
+  // for all strings with length less than 3, just return it (1, 2 etc.)
+  // also return the same text if the selected country has no fixed format
+  if ((text && text.length < 2) || !pattern) {
+    return prefix + text;
+  }
+
+  const formattedObject = pattern.split('').reduce((acc, character) => {
+    if (acc.remainingText.length === 0) {
+      return acc;
+    }
+
+    if (character !== '.') {
+      return {
+        formattedText: acc.formattedText + character,
+        remainingText: acc.remainingText,
+      };
+    }
+
+    const [head, ...tail] = acc.remainingText;
+
+    return {
+      formattedText: acc.formattedText + head,
+      remainingText: tail,
+    };
+  }, {
+    formattedText: '',
+    remainingText: text.split(''),
+  });
+
+  return formattedObject.formattedText;
+};
