70267a9fc8551b0d47fd71d53d65099fbb4cecc9
donskov <donskovdmitriyv@gmail.com>
2021-04-09 18:35:38 +0300

    fix: fix lint errors

--- packages/components/src/containers/portal/index.jsx
+++ packages/components/src/containers/portal/index.jsx
@@ -9,7 +9,7 @@ function getContainer(container) {
   return typeof container === 'function' ? container() : container;
 }
 
-const Portal = React.forwardRef((props, ref) => {
+const Portal = (props) => {
   const { children, container, onRendered } = props;
   const [mountNode, setMountNode] = React.useState(null);
 
@@ -22,7 +22,7 @@ const Portal = React.forwardRef((props, ref) => {
   }, [container, onRendered]);
 
   return mountNode ? ReactDOM.createPortal(children, mountNode) : mountNode;
-});
+};
 
 Portal.propTypes = {
   /**
